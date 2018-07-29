4ca2af5a83df02a33e54d0a5a4212559dab57be6
Florent <florent.buquet@gmail.com>
2018-07-29 18:12:14 +0200

    redirect

--- routes/index.js
+++ routes/index.js
@@ -2,25 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 //multer object creation
-var multer  = require('multer')
+var multer = require('multer')
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
+    destination: function(req, file, cb) {
         cb(null, 'public/uploads/')
     },
-    filename: function (req, file, cb) {
+    filename: function(req, file, cb) {
         cb(null, file.originalname)
     }
 });
 
-var upload = multer({ storage: storage })
+var upload = multer({ storage: storage });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Baptême de Julia' });
 });
 
-router.post('/', upload.single('imageupload'),function(req, res) {
-    res.send("File upload sucessfully.");
+router.post('/', upload.array('imageupload'), function(req, res) {
+    res.redirect('/?successMsg=Les fichiers ont bien été reçus.');
 });
 
 module.exports = router;
